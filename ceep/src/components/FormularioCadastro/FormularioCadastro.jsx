4fa73a2873029dc1938adcbdd18befc46807b944
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2021-08-30 23:35:41 -0300

    Update FormularioCadastro.jsx

--- ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -6,7 +6,7 @@ class FormularioCadastro extends Component {
     return (
       <form className="form-cadastro">
         <input type="text" placeholder="Título"  className="form-cadastro_input"/>
-        <textarea rows={15} placeholder="Escreva sua nota...." className="form-cadastro_input"> </textarea>
+        <textarea rows={15} placeholder="Escreva sua nota...." className="form-cadastro_input"></textarea>
         <button className="form-cadastro_input form-cadastro_submit"> Criar Nota </button>
       </form>
     );
